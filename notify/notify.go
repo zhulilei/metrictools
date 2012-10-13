0e3408dde20ba2cbfb4e664b70f14cf177a2d0f9
datastream <guxianjie@gmail.com>
2012-10-13 22:56:38 +0800

    update notify Send

--- notify/notify.go
+++ notify/notify.go
@@ -1,28 +1,58 @@
 package notify
 
 import (
+	"encoding/json"
 	"github.com/datastream/metrictools/types"
 	"log"
 )
 
-func Send(almaction types.AlarmAction, level int, value float64) {
-	for i := range almaction.Act {
-		switch almaction.Act[i] {
-		case "phone":
-			{
-				log.Println(almaction.Exp)
+type Message struct {
+	Product string //blog, photo
+	Type    string //nginx_cpu, nginx_req, app_cpu
+	Level   int    //0 ok, 1 warning, 2 error
+}
+type Notify struct {
+	Alarmaction types.AlarmAction
+	Level       int
+	Value       float64
+}
+
+func Send(notify_chan chan *Notify, msg_chan chan []byte) {
+	for {
+		nt := <-notify_chan
+		almaction := nt.Alarmaction
+		for i := range almaction.Act {
+			var ac types.Action
+			if err := json.Unmarshal(almaction.Act[i], &ac); err != nil {
+				log.Println("Action encode error", err)
+				continue
 			}
-		case "email":
-			{
-				log.Println(almaction.Exp)
-			}
-		case "im":
-			{
-				log.Println(almaction.Exp)
-			}
-		case "mq":
-			{
-				log.Println(almaction.Exp)
+			switch ac.T {
+			case "phone":
+				{
+					log.Println(almaction.Exp)
+				}
+			case "email":
+				{
+					log.Println(almaction.Exp)
+				}
+			case "im":
+				{
+					log.Println(almaction.Exp)
+				}
+			case "mq":
+				{
+					msg := &Message{
+						Product: almaction.Pd,
+						Type:    almaction.Type,
+						Level:   almaction.Stat,
+					}
+					if body, err := json.Marshal(msg); err == nil {
+						msg_chan <- body
+					} else {
+						log.Println("encode error: ", err)
+					}
+				}
 			}
 		}
 	}
