e99f5a90838e40192910b88f9034904dda04c69e
datastream <guxianjie@gmail.com>
2013-03-22 16:49:22 +0800

    sync code with http_to_nsq
    it will be remove, once nsq provide nsq.Writer

--- nsqclient.go
+++ nsqclient.go
@@ -1,4 +1,4 @@
-package metrictools
+package main
 
 import (
 	"bufio"
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-// define write message struct
+// define internal write message struct
 type WMessage struct {
 	Topic string
 	Body  interface{}
@@ -21,25 +21,21 @@ type WMessage struct {
 type Writer struct {
 	MessageChan chan WMessage
 	net.Conn
+	islive bool
 }
 
 func NewWriter(addr string) *Writer {
 	this := &Writer{
 		MessageChan: make(chan WMessage),
 	}
-	var err error
-	this.Conn, err = net.DialTimeout("tcp", addr, time.Second*5)
-	if err != nil {
-		log.Println("connect failed:", err)
-		return nil
-	}
-	go this.WriteLoop()
+	this.islive = true
+	go this.NewWriteLoop(addr)
 	return this
 }
 
+// stop writerloop
 func (this *Writer) Stop() {
 	close(this.MessageChan)
-	this.Conn.Close()
 }
 
 // body should be []byte or [][]byte
@@ -49,19 +45,53 @@ func (this *Writer) Write(topic string, body interface{}) error {
 		Body:  body,
 		Stat:  make(chan error),
 	}
+	if !this.islive {
+		return errors.New("WriteLoop exited")
+	}
 	this.MessageChan <- msg
-	return <-msg.Stat
+	err := <-msg.Stat
+	if err != nil {
+		if err.Error() != "Body not acceptable" {
+			this.MessageChan <- msg
+			err = <-msg.Stat
+		}
+	}
+	return err
 }
 
-// send message to nsqd node
-func (this *Writer) WriteLoop() {
+// if client can't connect nsqd, just exit loop. User should change nsqd_address or just recall NewWriteLoop()
+func (this *Writer) NewWriteLoop(addr string) {
+	go this.heartbeat()
+	for {
+		log.Println("Start WriteLoop")
+		if this.islive {
+			if err := this.writeloop(addr); err != nil {
+				log.Println(err)
+			}
+		} else {
+			break
+		}
+		log.Println("WriterLoop stopped")
+	}
+}
+
+// write loop until tcp error
+func (this *Writer) writeloop(addr string) error {
+	var err error
+	this.Conn, err = net.DialTimeout("tcp", addr, time.Second*5)
+	defer this.Conn.Close()
+	if err != nil {
+		this.islive = false
+		return err
+	}
 	this.Conn.Write(nsq.MagicV2)
 	rwbuf := bufio.NewReadWriter(bufio.NewReader(this.Conn),
 		bufio.NewWriter(this.Conn))
-	var err error
 	for {
 		msg, ok := <-this.MessageChan
+		isnop := false
 		if !ok {
+			this.islive = false
 			break
 		}
 		var cmd *nsq.Command
@@ -70,8 +100,12 @@ func (this *Writer) WriteLoop() {
 			cmd = nsq.Publish(msg.Topic, msg.Body.([]byte))
 		case [][]byte:
 			cmd, err = nsq.MultiPublish(msg.Topic, msg.Body.([][]byte))
+		case error:
+			cmd = nsq.Nop()
+			isnop = true
 		default:
-			err = errors.New("wrong data format")
+			msg.Stat <- errors.New("Body not acceptable")
+			continue
 		}
 		if err != nil {
 			msg.Stat <- err
@@ -87,6 +121,10 @@ func (this *Writer) WriteLoop() {
 			msg.Stat <- err
 			break
 		}
+		if isnop {
+			msg.Stat <- nil
+			continue
+		}
 		resp, err := nsq.ReadResponse(rwbuf)
 		if err != nil {
 			log.Println("failed to read response", err)
@@ -96,13 +134,33 @@ func (this *Writer) WriteLoop() {
 		_, data, err := nsq.UnpackResponse(resp)
 		if err != nil {
 			log.Println("unpack failed", err)
+			msg.Stat <- err
 			continue
 		}
 		if !bytes.Equal(data, []byte("OK")) &&
 			!bytes.Equal(data, []byte("_heartbeat_")) {
 			log.Println("response not ok", string(data))
-			continue
+			err = errors.New(string(data))
 		}
 		msg.Stat <- err
+	}
+	return nil
+}
+
+// send nop every 30s
+func (this *Writer) heartbeat() {
+	tick := time.Tick(time.Second * 30)
+	for {
+		<-tick
+		if !this.islive {
+			break
+		}
+		msg := WMessage{
+			Topic: "",
+			Body:  errors.New(""),
+			Stat:  make(chan error),
+		}
+		this.MessageChan <- msg
+		<-msg.Stat
 	}
 }
