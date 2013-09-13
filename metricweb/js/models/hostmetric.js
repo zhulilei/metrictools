e20d8d001c04a3582e1c69310117f62aca375a2f
datastream <guxianjie@gmail.com>
2013-09-13 14:46:52 +0800

    move to github.com/datastream/metricweb

--- metricweb/js/models/hostmetric.js
+++
@@ -1,9 +0,0 @@
-/*global Backbone */
-var app = app || {};
-
-(function () {
-    'use strict';
-
-    app.HostMetric = Backbone.Model.extend({
-    });
-})();
--- metricweb/js/models/metric.js
+++
@@ -1,9 +0,0 @@
-/*global Backbone */
-var app = app || {};
-
-(function () {
-    'use strict';
-
-    app.Metric = Backbone.Model.extend({
-    });
-})();
