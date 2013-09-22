0164a2151a0ec9a735fa55df41f20d44d15b0d9f
datastream <guxianjie@gmail.com>
2013-09-22 11:36:38 +0800

    init support skyline

--- metricstatistic/statistic.go
+++ metricstatistic/statistic.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/bitly/nsq/nsq"
 	"github.com/datastream/cal"
+	"github.com/datastream/skyline"
 	"github.com/garyburd/redigo/redis"
 	"log"
 	"strconv"
@@ -47,6 +48,55 @@ func (this *TriggerTask) calculate(trigger_name string) {
 	body := fmt.Sprintf("%d:%.2f", t, v)
 	_, err = data_con.Do("ZADD", "archive:"+trigger.Name, t, body)
 	_, err = data_con.Do("ZREMRANGEBYSCORE", "archive:"+trigger.Name, 0, t-3600)
+	values, err := redis.Strings(data_con.Do("ZRANGEBYSCORE", "archive:"+trigger.Name, t - 3600*3 , t))
+	if err != nil {
+		timeseries := ParseTimeSeries(values)
+		if skyline.MedianAbsoluteDeviation(timeseries) {
+			log.Println("medianabsolutedeviation:", trigger.Name)
+		}
+		if skyline.Grubbs(timeseries) {
+			log.Println("grubbs:", trigger.Name)
+		}
+		var one_hour []skyline.TimePoint
+		l := len(timeseries)
+		if  l > 60 {
+			one_hour = timeseries[l-60:l]
+		}
+		if skyline.FirstHourAverage(one_hour, 0) {
+			log.Println("firsthouraverage:", trigger.Name)
+		}
+		if skyline.SimpleStddevFromMovingAverage(timeseries) {
+			log.Println("simplestddevfrommovingaverage:", trigger.Name)
+		}
+		if skyline.StddevFromMovingAverage(timeseries) {
+			log.Println("stddevfrommovingaverage:", trigger.Name)
+		}
+		if skyline.MeanSubtractionCumulation(timeseries) {
+			log.Println("meansubtractioncumulation:", trigger.Name)
+		}
+		if skyline.LeastSquares(timeseries) {
+			log.Println("leastsquares:", trigger.Name)
+		}
+		if skyline.HistogramBins(timeseries) {
+			log.Println("histogram:", trigger.Name)
+		}
+	}
+}
+
+func ParseTimeSeries(values []string) []skyline.TimePoint {
+	var rst []skyline.TimePoint
+	for _, val := range values {
+		t, v , err := metrictools.GetTimestampAndValue(val)
+		if err != nil {
+			continue
+		}
+		timepoint := skyline.TimePoint{
+			Timestamp: t,
+			Value: v,
+		}
+		rst = append(rst, timepoint)
+	}
+	return rst
 }
 
 func calculate_exp(t *TriggerTask, exp string) (float64, error) {
