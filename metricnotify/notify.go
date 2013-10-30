41a9e7eca4239722c877f6cff5af95750ba7973e
datastream <guxianjie@gmail.com>
2013-10-30 19:06:26 +0800

    fix notify

--- metricnotify/notify.go
+++ metricnotify/notify.go
@@ -32,9 +32,9 @@ func (m *Notify) HandleMessage(msg *nsq.Message) error {
 func (m *Notify) sendNotify(notifyMsg map[string]string) {
 	configCon := m.Get()
 	defer configCon.Close()
-	keys, err := redis.Strings(configCon.Do("SMEMBERS", notifyMsg["trigger"]+":actions"))
+	keys, err := redis.Strings(configCon.Do("SMEMBERS", notifyMsg["trigger_exp"]+":actions"))
 	if err != nil {
-		log.Println("no action for", notifyMsg["trigger"])
+		log.Println("no action for", notifyMsg["trigger_exp"])
 		return
 	}
 	for _, v := range keys {
